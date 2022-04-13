a0267e98fe314c993246f268a3f939f5d9826f78
Wesley Eenjes <P7uisdev@gmail.com>
2022-04-13 14:05:11 +0200

    Added PID code

--- Code/BallvalveServo/BallvalveServo.ino
+++ Code/BallvalveServo/BallvalveServo.ino
@@ -1,11 +1,19 @@
+#include <PID_v1.h>
 #include <Servo.h>
 Servo servo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
-int counter =0;
+int valvepos =0;
+int setpos = 0;
 #include "thermistor.h"
 
 // Analog pin used to read the NTC
 #define NTC_PIN A7
+
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=2, Ki=5, Kd=1;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 // Thermistor object
 THERMISTOR thermistor(NTC_PIN,        // Analog pin
@@ -18,14 +26,26 @@ uint16_t temp;
 
 void setup() {
   Serial.begin(9600);
-  Serial.print(counter);
-  //HOME();
+  Serial.print(valvepos);
+
+  //initialize the variables we're linked to
+  Input = temp;
+  Setpoint = 50;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+  
+  //HOME(); 
 }
 
 void loop() {
   //Serial.print(counter);
   TEMPREAD();
-  delay(2000);
+  PIDC();
+  SETVALVE();
+  delay(500);
+  Serial.print("setpos ");
+  Serial.println(setpos);
   
 }
 
@@ -36,13 +56,13 @@ void loop() {
   }
   
   void OPEN(){
-    for (counter = 1; counter <= 33;){
+    for (valvepos = 1; valvepos <= 33;){
       open1();
       delay(200);
     }
   }
   void CLOSE(){
-    for (counter = 33; counter >= 1;){
+    for (valvepos = 33; valvepos >= 1;){
       close1();
       delay(200);
     }
@@ -55,7 +75,9 @@ void loop() {
     servo.write(0);
     delay(100);
     servo.detach();
-    counter = counter +1;
+    valvepos = valvepos +1;
+    Serial.print("valvepos: ");
+    Serial.println(valvepos);
   }
 
   void close1(){
@@ -64,12 +86,37 @@ void loop() {
     servo.write(180);
     delay(100);
     servo.detach();
-    counter = counter -1;
+    valvepos = valvepos -1;
+    Serial.print("valvepos: ");
+    Serial.println(valvepos);
   }
 
 void TEMPREAD(){
 temp = thermistor.read();   // Read temperature
   Serial.println (temp);
-  
+}
 
+
+void PIDC(){
+    Input = temp;
+    myPID.Compute();
+   Serial.print("PIDOUT: ");
+   Serial.println(Output);
+   setpos = map(Output, 0, 255, 33, 0);
+}
+
+void SETVALVE(){
+  if (setpos > valvepos) {
+    while (setpos > valvepos) {
+      open1();
+    }
+}
+  else if (setpos < valvepos) {
+    while (setpos < valvepos) {
+      close1();
+    }
+}
+  else {
+  // Safe! Continue usual tasks.
+  }
 }
